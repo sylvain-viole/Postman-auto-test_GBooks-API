8b8ce99cc9c7012df66a4ab590e1c4941b3bca68
Sylvain Viole <sylvainviole@gmail.com>
2021-04-05 09:43:04 +0200

    update testscript.sh

--- testScript.sh
+++ testScript.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+newman run Google\ Books\ Api\ Test.postman_collection.json -d datas.json -r htmlextra --reporter-htmlextra-title "Google Books Api Testing Report"
 git status
+git add newman/
+git commit -m "Test Result on $(date)"
