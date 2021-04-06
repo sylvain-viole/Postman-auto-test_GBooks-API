b1987be4fb9cda2337714093382b2409dcedcc76
Sylvain Viole <sylvainviole@gmail.com>
2021-04-06 17:04:49 +0200

    delete reports

--- launchTest.sh
+++ launchTest.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-newman run Google\ Books\ Api\ Test.postman_collection.json -d datas.json -r htmlextra --reporter-htmlextra-title "Google Books Api Testing Report"
-git status
+newman run https://www.getpostman.com/collections/d17eab280931854df69a -d datas.json -e TEST.postman_environment.json -r htmlextra --reporter-htmlextra-title "Google Books Api Testing Report"
+sleep 1
 git add newman/
+sleep 1
 git commit -m "Test Result on $(date)"
+sleep 1
 git push origin main
