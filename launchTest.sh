5537c5654b277e16b4b33dce3cf8b704e202e4bc
Sylvain Viole <sylvainviole@gmail.com>
2021-04-09 15:23:22 +0200

    Test Result on Ven  9 avr 2021 15:23:22 CEST

--- launchTest.sh
+++ launchTest.sh
@@ -1,9 +1,7 @@
 !/bin/bash
 newman run gbook_test.postman_collection.json -d gbook_test.postman_datas.json -e gbook_test.postman_datas.json -r htmlextra --reporter-htmlextra-title "Google Books Api Testing Report"
 sleep 1
-cd newman
 git add .
-cd ..
 sleep 1
 git commit -m "Test Result on $(date)"
 sleep 1
