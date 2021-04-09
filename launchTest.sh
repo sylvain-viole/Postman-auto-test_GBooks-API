c6649e61e2a6299c06632e9cfdf23113c55cdc98
Sylvain Viole <sylvainviole@gmail.com>
2021-04-09 18:53:32 +0200

    remove sleep from lauchTest.sh

--- launchTest.sh
+++ launchTest.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 newman run gbook_test.postman_collection.json -d gbook_test.postman_datas.json -e gbook_test.postman_datas.json -r htmlextra --reporter-htmlextra-title "Google Books Api Testing Report"
-sleep 1
 git add newman
-sleep 1
 git commit -m "Test Result on $(date)"
-sleep 1
 git push origin main
